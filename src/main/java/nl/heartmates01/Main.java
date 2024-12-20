dedcaa547c56c49440030e88b7fbc7a7b0143cb2
heartmates01 <kiryuchameke@gmail.com>
2024-12-20 12:07:11 +0100

    overhaul

--- src/main/java/nl/heartmates01/Main.java
+++ src/main/java/nl/heartmates01/Main.java
@@ -1,20 +1,5 @@
 package nl.heartmates01;
 
-// Try This 2-1
-// Compute the distance to a lightning strike
-// whose sound take 7.2 seconds to reach you.
-
-//class Sound {
-//
-//  public static void main(String[] args) {
-//    double dist;
-//
-//    dist = 7.2 * 1100;
-//
-//    System.out.println("The lightning is " + dist + " feet away.");
-//  }
-//}
-
 // Geluid verplaatst zich met ongeveer 343 meter per seconden.
 // Wanneer je een flits ziet en telt tot de donder,
 // dan weet je de afstand tot de bliksem afstand in meters = tijd in seconden X 343 meter
@@ -34,46 +19,37 @@ package nl.heartmates01;
 // afstand: ms x 343, ft x 1100
 // duurt: ms x 5, ft x 26400
 
-
 import java.util.Scanner;
 
 class Sound {
 
   public static void main(String[] args) {
-
-    Scanner FtKm = new Scanner(System.in);
-    System.out.println("Calculate in FT or MS?\nFT = 1\nMS = 2");
-    int Choice = FtKm.nextInt();
-
-    if (Choice == 1) {
-      while (true) {
-        Scanner SecondsFT = new Scanner(System.in);
-        System.out.println(
-            "How many seconds are there between the thunder and lightning? To quit, type '0'.");
-        int givenSecondsFT = SecondsFT.nextInt();
-
-        if (givenSecondsFT == 0) {
-          System.out.println("Bye!");
-          System.exit(0);
-        } else {
-          int distFT = givenSecondsFT * 1100;
-          System.out.println("The lightning is " + distFT + " feet away.");
-        }
-      }
-    } else if (Choice == 2) {
-      while (true) {
-        Scanner SecondsMS = new Scanner(System.in);
-        System.out.println(
-            "How many seconds are there between the thunder and lightning? To quit, type '0'.");
-        int givenSecondsMS = SecondsMS.nextInt();
-
-        if (givenSecondsMS == 0) {
-          System.out.println("Bye!");
-          System.exit(0);
-        } else {
-          int distMS = givenSecondsMS * 343;
-          System.out.println("The lightning is " + distMS + " meters away.");
-        }
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Calculate in FT or MS?\n1. FT\n2.MS");
+    int choice = scanner.nextInt();
+    
+    int distance = 0;
+    int time = 0;
+    
+    if (choice == 1) {
+      distance = 1100;
+      time = 26400;
+    } else if (choice == 2) {
+      distance = 343;
+      time = 5;
+    }
+    
+    while(true){
+      System.out.println("How many seconds are there between the thunder and the lightning? To quit, type '0'.");
+      int seconds = scanner.nextInt();
+      if(seconds == 0){
+        System.out.println("Exiting.");
+        System.exit(0);
+      } else {
+        int distanceResult = seconds * distance;
+        int timeResult = seconds * time;
+        System.out.println("The lightning is " + distanceResult + " feet away, and will take " + timeResult + " to reach you.");
       }
     }
   }
